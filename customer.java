69dc3c860ed61278a212d47c40fc685de2e45a02
ismailyraza <98678304+ismailyraza@users.noreply.github.com>
2023-08-23 09:25:32 +0530

    Create customer.java

---
+++ customer.java
@@ -0,0 +1,11 @@
+public class Customer {
+    private String name;
+    private String city;
+
+    public Customer(String name, String city) {
+        this.name = name;
+        this.city = city;
+    }
+
+    // Getters and setters
+}
