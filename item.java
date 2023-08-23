034bf26e67196bef394aa1a7cebc754f27fb7ee9
ismailyraza <98678304+ismailyraza@users.noreply.github.com>
2023-08-23 09:25:08 +0530

    item.java

---
+++ item.java
@@ -0,0 +1,11 @@
+public class Item {
+    private String name;
+    private double price;
+
+    public Item(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    // Getters and setters
+}
